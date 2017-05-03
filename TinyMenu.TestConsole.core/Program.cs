d4c8097dfdb1fb00f873f9adb9f730e5370226a7
Richard Dutton <Richard Dutton>
2017-05-03 21:33:26 +0100

    Add project files.

---
+++ TinyMenu.TestConsole.core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyMenu.TestConsole.core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TinyMenu/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TinyMenu
+{
+    public class Class1
+    {
+    }
+}
